4bf58025417e2333a360ac594ac6ef8737a57199
bitVil <54509903+bitVil@users.noreply.github.com>
2019-09-20 15:30:39 +0100

    Create Outcast.java

---
+++ wordnet/Outcast.java
@@ -0,0 +1,42 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Outcast {
+    private WordNet wordnet;
+
+    // constructor takes a WordNet object
+    public Outcast(WordNet wordnet) {
+        this.wordnet = wordnet;
+    }
+
+    // given an array of WordNet nouns, return an outcast
+    public String outcast(String[] nouns) {
+        int[] distances = new int[nouns.length];
+        for (int i = 0; i < nouns.length; i++) {
+            for (int j = 0; j < nouns.length; j++) {
+                distances[i] += wordnet.distance(nouns[i], nouns[j]);
+            }
+        }
+        int max = 0;
+        for (int i = 1; i < nouns.length; i++) {
+            if (distances[i] > distances[max]) max = i;
+        }
+        return nouns[max];
+    }
+
+    public static void main(String[] args) {
+        WordNet wordnet = new WordNet(args[0], args[1]);
+        Outcast outcast = new Outcast(wordnet);
+        for (int t = 2; t < args.length; t++) {
+            In in = new In(args[t]);
+            String[] nouns = in.readAllStrings();
+            StdOut.println(args[t] + ": " + outcast.outcast(nouns));
+        }
+    }
+}
