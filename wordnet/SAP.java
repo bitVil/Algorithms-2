8e005112d787efab886281b1e5848aa6c1d56ea2
bitVil <54509903+bitVil@users.noreply.github.com>
2019-09-20 15:30:36 +0100

    Create SAP.java

---
+++ wordnet/SAP.java
@@ -0,0 +1,105 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.BreadthFirstDirectedPaths;
+import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class SAP {
+    private Digraph dg;
+
+    // constructor takes a digraph, not necessarily a DAG.
+    public SAP(Digraph dg) {
+        this.dg = new Digraph(dg);
+    }
+
+    // length of shortest ancestral path between v and w; -1 if no such path
+    public int length(int v, int w) {
+        BreadthFirstDirectedPaths vPaths = new BreadthFirstDirectedPaths(dg, v);
+        BreadthFirstDirectedPaths wPaths = new BreadthFirstDirectedPaths(dg, w);
+        int minDist = -1;
+        for (int i = 0; i < dg.V(); i++) {
+            if (vPaths.hasPathTo(i) && wPaths.hasPathTo(i)) {
+                int dist = vPaths.distTo(i) + wPaths.distTo(i);
+                if (minDist == -1 || minDist > dist) minDist = dist;
+            }
+        }
+        return minDist;
+    }
+
+    // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
+    public int ancestor(int v, int w) {
+        BreadthFirstDirectedPaths vPaths = new BreadthFirstDirectedPaths(dg, v);
+        BreadthFirstDirectedPaths wPaths = new BreadthFirstDirectedPaths(dg, w);
+        int minDist = -1;
+        int ancestor = -1;
+        for (int i = 0; i < dg.V(); i++) {
+            if (vPaths.hasPathTo(i) && wPaths.hasPathTo(i)) {
+                int dist = vPaths.distTo(i) + wPaths.distTo(i) + 1;
+                if (minDist == -1 || minDist > dist) {
+                    minDist = dist;
+                    ancestor = i;
+                }
+            }
+        }
+        return ancestor;
+    }
+
+    // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
+    public int length(Iterable<Integer> v, Iterable<Integer> w) {
+        if (v == null || w == null) throw new IllegalArgumentException();
+        for (Integer n : v) if (n == null) throw new IllegalArgumentException();
+        for (Integer n : w) if (n == null) throw new IllegalArgumentException();
+        BreadthFirstDirectedPaths vPaths = new BreadthFirstDirectedPaths(dg, v);
+        BreadthFirstDirectedPaths wPaths = new BreadthFirstDirectedPaths(dg, w);
+        int minDist = -1;
+        for (int i = 0; i < dg.V(); i++) {
+            if (vPaths.hasPathTo(i) && wPaths.hasPathTo(i)) {
+                int dist = vPaths.distTo(i) + wPaths.distTo(i);
+                if (minDist == -1 || minDist > dist) {
+                    minDist = dist;
+                }
+            }
+        }
+        return minDist;
+    }
+
+    // a common ancestor that participates in shortest ancestral path; -1 if no such path
+    public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
+        if (v == null || w == null) throw new IllegalArgumentException();
+        for (Integer n : v) if (n == null) throw new IllegalArgumentException();
+        for (Integer n : w) if (n == null) throw new IllegalArgumentException();
+        BreadthFirstDirectedPaths vPaths = new BreadthFirstDirectedPaths(dg, v);
+        BreadthFirstDirectedPaths wPaths = new BreadthFirstDirectedPaths(dg, w);
+        int minDist = -1;
+        int ancestor = -1;
+        for (int i = 0; i < dg.V(); i++) {
+            if (vPaths.hasPathTo(i) && wPaths.hasPathTo(i)) {
+                int dist = vPaths.distTo(i) + wPaths.distTo(i) + 1;
+                if (minDist == -1 || minDist > dist) {
+                    minDist = dist;
+                    ancestor = i;
+                }
+            }
+        }
+        return ancestor;
+    }
+
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        Digraph G = new Digraph(in);
+        SAP sap = new SAP(G);
+        while (!StdIn.isEmpty()) {
+            int v = StdIn.readInt();
+            int w = StdIn.readInt();
+            int length = sap.length(v, w);
+            int ancestor = sap.ancestor(v, w);
+            StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
+        }
+    }
+}
